c5651b0337d904ab5a8c9d9b36a16c21ffbe92b8
Hurry Burton <buzdiganoff@gmail.com>
2023-10-07 22:29:06 +0300

    todo update feature

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,11 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_comleted=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_comleted=True)
+    
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks, 
     }
     return render(request, 'home.html', context)
